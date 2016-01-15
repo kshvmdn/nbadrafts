16120350c9cdf1b66a3b5e0215f5a9012af07a67
Kashav Madan <kshvmdn@gmail.com>
2016-01-15 01:35:54 -0500

    Refactored file loop, writerow() → writerows()
    List comprehension ❤️

--- create_csv.py
+++ create_csv.py
@@ -1,15 +1,14 @@
-import sys, csv
+import csv
 from datetime import date
 
 from scraper import scrape
 
 CSV_FILE = 'datasets/%s_nbadraft.csv'
 
-for year in range(1947, date.today().year):
+for year in range(2015, date.today().year):
     draft = scrape(year)
     header = [key for key in draft[1].keys()]
     with open(CSV_FILE % year, 'w', newline='') as outfile:
         dw = csv.DictWriter(outfile, header)
         dw.writeheader()
-        for i, row in draft.items():
-            dw.writerow(row)
+        dw.writerows([row for index, row in draft.items()])
