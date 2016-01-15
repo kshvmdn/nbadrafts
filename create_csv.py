f25bbfd21b38921b0317e4982a31a21a6ba43af6
Kashav Madan <kshvmdn@gmail.com>
2016-01-15 01:41:41 -0500

    Fix year range 🙃

--- create_csv.py
+++ create_csv.py
@@ -5,7 +5,7 @@ from scraper.draft_scraper import scrape
 
 CSV_FILE = 'datasets/%s_nbadraft.csv'
 
-for year in range(2015, date.today().year):
+for year in range(1947, date.today().year):
     draft = scrape(year)
     header = [key for key in draft[1].keys()]
     with open(CSV_FILE % year, 'w', newline='') as outfile:
