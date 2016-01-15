d98dc7e1d96f020ccdef08b9bf892aeff1233288
Kashav Madan <kshvmdn@gmail.com>
2016-01-15 01:27:33 -0500

    Persist dict order when writing headers to csv

--- assemble_csv.py
+++ assemble_csv.py
@@ -3,13 +3,13 @@ from datetime import date
 
 from scraper import scrape
 
-CSV_FILE = 'datasets/%s-DRAFT.csv'
+CSV_FILE = 'datasets/%s_nbadraft.csv'
 
 for year in range(1947, date.today().year):
-    print(year)
     draft = scrape(year)
-    with open(CSV_FILE % year, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=dict(draft[1]).keys())
-        writer.writeheader()
+    header = [key for key in draft[1].keys()]
+    with open(CSV_FILE % year, 'w', newline='') as outfile:
+        dw = csv.DictWriter(outfile, header)
+        dw.writeheader()
         for i, row in draft.items():
-            writer.writerow(row)
+            dw.writerow(row)
