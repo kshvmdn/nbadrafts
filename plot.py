e0e64e12e2f829a4200aeb04d6fd94860c1736dc
Kashav Madan <kshvmdn@gmail.com>
2016-01-19 15:44:26 -0500

    Add populator for players, init. plot function

--- plot.py
+++ plot.py
@@ -1,5 +1,36 @@
-import numpy as np
+import collections
+import csv
 import matplotlib.pyplot as plt
-import pylab
+import pprint
+
+CSV_PATH = 'datasets/%s_nbadraft.csv'
 
 
+def plot(points, pick):
+    n, bins, patches = plt.hist(karma, 100, facecolor='green', alpha=0.75)
+
+    plt.xlabel('Karma')
+    plt.ylabel('Post count')
+    plt.gca().set_yscale('log')
+    plt.grid(True)
+
+    plt.show()
+
+
+players = []
+
+def populate_players(year):
+    player = {}
+    with open(CSV_PATH % str(year)) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # score scale (stat, worth)
+            # pt, 1
+            # ast, 2
+            # rbd, 4
+            player_details = [row['player'], row['ranker'], row['pts_per_g'], row['trb_per_g'], row['ast_per_g']]
+            players.append(player_details)
+
+
+populate_player_dict(2015)
+pprint.pprint(players)
